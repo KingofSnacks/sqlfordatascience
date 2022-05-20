e4128b7e6b89ddb1dde6a9d43dcf80b235fa6451
KingofSnacks <101528200+KingofSnacks@users.noreply.github.com>
2022-05-20 08:52:51 -0700

    Update mod3ass.sql

--- mod3ass.sql
+++ mod3ass.sql
@@ -42,3 +42,31 @@ JOIN
     FROM Employees
     WHERE Title LIKE '%Manager%') AS t1
 ON e.ReportsTo = t1.EmployeeId
+
+--Find the name and ID of the artists who do not have albums. 
+
+SELECT a.Name, a.ArtistId
+FROM Artists AS a
+LEFT JOIN
+    (SELECT ArtistId
+    FROM Albums
+    GROUP BY ArtistId) AS t1
+ON a.ArtistId = t1.ArtistId
+WHERE t1.ArtistId IS null
+
+--Use a UNION to create a list of all the employee's and customer's first names and last names ordered by the last name in descending order.
+
+SELECT FirstName, LastName
+FROM Employees
+UNION
+SELECT FirstName, LastName
+FROM Customers
+ORDER BY LastName desc
+
+--See if there are any customers who have a different city listed in their billing city versus their customer city.
+
+SELECT c.CustomerId, c.City AS CustomerCity, i.BillingCity
+FROM Customers AS c
+JOIN INVOICES AS i
+ON c.CustomerId = i.CustomerId
+WHERE CustomerCity <> BillingCity
